804c9ed01da0f3c5fb695c1713de1fbe74671677
Tracy Joshua <tracyjoshua2007@gmail.com>
2025-09-29 22:12:51 +0300

    Extract date

--- JS/index.js
+++ JS/index.js
@@ -1,7 +1,34 @@
 function getAkanName(){
     console.log("Clicked the button");
-    const date = document.getElementById("birthdate").value;
+    const dateInput = document.getElementById("birthdate");
 
-    console.log(date);
+    if(dateInput == null){
+        alert("Date input does not exist")
+        return;
+    }
+
+    const dateValue = dateInput.value;
+    const validation = validateDate(dateValue);
+
+   
+    // const gender = document.querySelector('input[name="gender"]:checked').value;
+
+    // validateDate(date);
+
+
+    // console.log(date);
+
+}
+
+function validateDate(date){
+    // 2025-09-21
+
+    const year = date.substring(0, 4);
+    const month = date.substring(5, 7);
+    const day = date.substring(8, 10);
+
+    console.log(year);
+    console.log(month);
+    console.log(day)
 
 }
